522dd4e789824ceb26aa6bde77ae5633b42ba51d
Alex Bahn <AanderBahn@gmail.com>
2022-06-23 18:03:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,37 @@ var canvas = document.getElementById('myCanvas');
 
 var ctx    = canvas.getContext('2d');
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+setInterval( draw, 10);
+
+var x = canvas.width/2;
+var y = canvas.height - 30;
+var dx =  2;
+var dy = -2;
+var ballRadius = 10;
+
+function drawBall () {
+	ctx.beginPath();
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+	ctx.fillStyle = "blue";
+	ctx.fill();
+	ctx.closePath();
+}
+
+function draw() {
+	ctx.clearRect( 0, 0, canvas.width, canvas.height);
+	drawBall();
+
+
+	if(y + dy > canvas.height || y + dy < 0) {
+		dy = -dy;
+	}
+	if(x + dx > canvas.width || x + dx < 0) {
+		dx = -dx;
+	}
+
+
+	
+
+	x += dx;
+	y += dy;
+}
